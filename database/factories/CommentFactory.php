c7ddc7e36d6ec045b5e9c265866a0dd0377fe358
Czedrix Barcena <czedrixb@gmail.com>
2024-08-19 15:58:32 +0800

    added comment seeder for factory practice

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    protected $model = Comment::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->email(),
+            'body' => $this->faker->sentence(45),
+            'approved' => $this->faker->boolean(),
+            'likes' => $this->faker->randomNumber(5),
+        ];
+    }
+}
